98b34be998cdae7d2cd5149f7bc2f8173bcec022
canyapalak <canyapalak@gmail.com>
2023-01-29 01:36:41 +0100

    now you can post messages in chat room

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,4 +1,12 @@
-import { collection, getDocs, query } from "firebase/firestore";
+import { Link } from "@mui/icons-material";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
@@ -6,42 +14,70 @@ import { db } from "../config/FirebaseConfig";
 import { AuthContext } from "../store/AuthContext";
 
 function ChatComponent() {
-  console.log("db :>> ", db);
+  const { user } = useContext(AuthContext);
   const [messages, setMessages] = useState([]);
-  const [user, setUser] = useState(null);
+  const [text, setText] = useState("");
+  const [notLoggedIn, setNotLoggedIn] = useState(null);
+  const redirectTo = useNavigate;
 
-  const getMessages = async () => {
-    const q = query(collection(db, "chatroom"));
+  useEffect(() => {
+    getUpdatedMessages();
+  }, []);
 
-    const querySnapshot = await getDocs(q);
-    const msgs = [];
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
-      msgs.push(doc.data());
+  const getUpdatedMessages = () => {
+    const q = query(collection(db, "chatroom"), orderBy("date", "asc"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const msgs = [];
+      querySnapshot.forEach((doc) => {
+        msgs.push(doc.data());
+      });
+      setMessages(msgs);
     });
-    setMessages(msgs);
   };
 
   const msgDate = (dateAndTime) => {
     const date = new Date(dateAndTime * 1000).toLocaleDateString();
     const time = new Date(dateAndTime * 1000).toLocaleTimeString();
     return (
-      <p>
+      <>
         {date} {time}
-      </p>
+      </>
     );
   };
 
-  useEffect(() => {
-    getMessages();
-  }, []);
+  //add new comment
+
+  const handleMessageInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmitMessage = async () => {
+    try {
+      const msgObj = {
+        text: text,
+        author: user.email,
+        date: new Date(),
+      };
+      const docRef = await addDoc(collection(db, "chatroom"), msgObj);
+      console.log("Document written with ID: ", docRef.id);
+      notLoggedIn && redirectTo("/login");
+    } catch (error) {
+      console.log("post message error :>> ", error);
+      const errorMessage = error.message;
+      if (errorMessage.includes("addDoc() called with invalid data")) {
+        setNotLoggedIn(true);
+      } else {
+        setNotLoggedIn(false);
+      }
+    }
+  };
+
+  console.log("messages", messages);
 
   return (
     <div className="chatroom-container">
       <div className="be-comment-block">
         <h1 className="comments-title">Chat Room</h1>
-
         {messages &&
           messages.map((message, index) => {
             return (
@@ -51,7 +87,7 @@ function ChatComponent() {
                     <p>{message.author}</p>
                   </span>
                   <span className="be-comment-time">
-                    <p>{msgDate(message.date.seconds)}</p>
+                    <p>{msgDate(message.date)}</p>
                   </span>
                   <p className="be-comment-text">{message.text}</p>
                 </div>
@@ -68,14 +104,14 @@ function ChatComponent() {
                     className="form-input"
                     required=""
                     placeholder="Write a comment..."
-                    // onChange={handleCommentInput}
+                    onChange={handleMessageInput}
                   ></textarea>
                 </div>
               </div>
               <Button
                 variant="outline-success"
                 className="chatroom-button"
-                // onClick={postComment}
+                onClick={handleSubmitMessage}
               >
                 Submit
               </Button>
